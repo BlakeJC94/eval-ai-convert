442c44589f8768f317c165c550635d01972da4b9
BlakeJC94 <blakejamescook@gmail.com>
2022-04-20 12:10:53 +1000

    sync

--- setup.py
+++ setup.py
@@ -12,19 +12,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="ea_convert",
-    version="0.0.1",
+    version="0.3.0",
     description='Python package for converting Eval AI dataset',
     url="https://github.com/BlakeJC94/eval-ai-convert",
     author="BlakeJC94",
     python_requires=">=3.8",
     packages=find_packages(),
     install_requires=[
+        'fire',
+        'mne',
         'numpy',
         'pandas',
-        'mne',
-        'tqdm',
         'pyarrow',
-        'fire',
+        'tqdm',
     ],
     entry_points={
         'console_scripts': ['ea-convert=ea_convert.__main__:main'],
