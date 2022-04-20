bdc789068c7a7ca4cfca58640dc7cac1cf099d33
BlakeJC94 <blakejamescook@gmail.com>
2022-04-20 10:58:09 +1000

    updated setup

--- setup.py
+++ setup.py
@@ -1,26 +1,32 @@
-import setuptools
+"""Setup script.
 
-setuptools.setup(
-    name="eval_ai_convert",
+Install package:
+    $ pip install -e .
+
+Requirements can be updated using `pip-compile`:
+    $ pip install pip-tools
+    $ pip-compile
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ea_convert",
     version="0.0.1",
     description='Python package for converting Eval AI dataset',
     url="https://github.com/BlakeJC94/eval-ai-convert",
-    author="Blake Cook",
-    classifiers=[
-        'Development Status :: Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ],
+    author="BlakeJC94",
     python_requires=">=3.8",
-    packages=setuptools.find_packages(),
-    package_data={'seeralgo': ['../output/*']},
+    packages=find_packages(),
     install_requires=[
         'numpy',
         'pandas',
         'mne',
         'tqdm',
+        'pyarrow',
+        'fire',
     ],
+    entry_points={
+        'console_scripts': ['ea-convert=ea_convert.__main__:main'],
+    },
 )
