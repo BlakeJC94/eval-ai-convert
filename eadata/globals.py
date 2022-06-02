13c7ea62a471a6cedd4f5f2708db9ec94a26c753
Blake <blakejamescook@gmail.com>
2022-06-02 15:06:05 +1000

    feat: updated splitting script to only output train/test split

--- eadata/globals.py
+++ eadata/globals.py
@@ -2,6 +2,6 @@ PATIENT_IDS = ['1110', '1869', '1876', '1904', '1965', '2002']
 DTYPES = ['ACC', 'BVP', 'EDA', 'HR', 'TEMP']
 SRATE = 128
 
-SPLIT_NAMES = ['train', 'test', 'val']
+SPLIT_NAMES = ['train', 'test']
 TIMESTAMP_FORMAT = "UTC-%Y_%m_%d-%H_%M_%S"
 
--- eadata/split.py
+++ eadata/split.py
@@ -1,5 +1,6 @@
 import shutil
 import logging
+from typing import List, Union
 
 import numpy as np
 
@@ -9,37 +10,37 @@ from .paths import PARQUET_PATH
 logger = logging.getLogger(__name__)
 
 
-def split(patient_id: str, train_prop: float, test_prop: float):
-    """Create train/test/val split across sessions in parquet.
+def split(patient_id: str, proportions: List[Union[float, int]]):
+    """Create train/test split across sessions in parquet.
 
     Requires converted data in parquet format, see `convert`.
 
-    Splits data in `./data/parquet/<pid>` by file size into `./data/parquet/<pid>/<split>`, where
-    `<split>` is either `train`, `test`, or `val`. Existing splits will be undone before creating
-    new split.
+    Splits data in `./data/parquet/<pid>` by file size into `./data/parquet/<split>/<pid>`, where
+    `<split>` is either `train`, or `test`. Existing splits will be undone before creating new
+    split.
 
     Args:
         patient_id: Patient ID.
-        train_prop: Proportion of data to use for training.
-        test_prop: Proportion of data to use for testing, remaining proportion for validation.
+        proportions: Proportions of data to use for testing, remaining proportion for training.
     """
-    assert train_prop > 0 and test_prop > 0
-    assert train_prop + test_prop <= 1.0, "Proportions must sum to 1.0"
-    assert str(patient_id) in PATIENT_IDS, "Patient ID not found"
+    assert all(p > 0 for p in proportions), "Expected valid `proportions`."
+    assert len(proportions) == 2, "Expected 2 proportions for train/test split"
+    assert str(patient_id) in PATIENT_IDS, "Patient ID not found."
 
     patient_path = PARQUET_PATH / str(patient_id)
 
     if not patient_path.exists() or not any(patient_path.iterdir()):
         raise FileNotFoundError('Convert data to parquet before running')
 
-    val_prop = 1 - (train_prop + test_prop)
-    logger.info(f"Creating train/test/val split of {train_prop} : {test_prop} : {val_prop}")
+    # normalise proportions
+    train_prop, test_prop = [p / sum(proportions) for p in proportions]
+    logger.info(f"Creating train/test/val split of {train_prop} : {test_prop}")
 
     # If sessions in PARQUET_DIR are already split, undo the split before proceeding
     for split_name in SPLIT_NAMES:
         split_path = PARQUET_PATH / split_name / str(patient_id)
         if split_path.exists():
-            logger.info(f"Found {split_name} split, undoing before proceeding")
+            logger.info(f"Found {split_name} split for {patient_id =}, undoing before proceeding")
             for session in split_path.iterdir():
                 shutil.move(str(session), str(patient_path))
             shutil.rmtree(str(split_path))
@@ -53,19 +54,13 @@ def split(patient_id: str, train_prop: float, test_prop: float):
     c_props = np.cumsum(session_sizes) / np.sum(session_sizes)
 
     # Split session_dirs into splits of given proportions
-    train_idx = np.where(c_props <= train_prop)[0][-1]
-    test_idx = np.where((c_props > train_prop) & (c_props <= train_prop + test_prop))[0][-1]
-    splits = [session_dirs[:train_idx], session_dirs[train_idx:test_idx], session_dirs[test_idx:]]
-    if test_idx < len(session_dirs):
-        splits.append(session_dirs[test_idx:])
-    else:
-        splits.append([])
-    splits = dict((name, dirs) for name, dirs in zip(SPLIT_NAMES, splits))
+    split_idx = np.where(c_props <= train_prop)[0][-1]
+    splits = [session_dirs[:split_idx], session_dirs[split_idx:]]
 
     # Move sessions to their splits
-    for split_name, session_dirs in splits.items():
+    for split_name, session_dirs in zip(SPLIT_NAMES, splits):
         split_path = PARQUET_PATH / split_name / str(patient_id)
-        split_path.mkdir(exist_ok=True)
+        split_path.mkdir(exist_ok=True, parents=True)
         logger.info(f"Creating {split_name} split")
 
         if len(session_dirs) == 0:
@@ -74,3 +69,7 @@ def split(patient_id: str, train_prop: float, test_prop: float):
 
         for session in session_dirs:
             shutil.move(str(session), str(split_path))
+
+    # Remove empty directories
+    if not any(patient_path.iterdir()):
+        patient_path.rmdir()
